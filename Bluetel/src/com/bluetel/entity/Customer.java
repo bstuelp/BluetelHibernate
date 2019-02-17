74d904fe83aca5f908c5ed0a470ff33341adc321
notebook\bstuelp <bstuelp@gmail.com>
2019-02-17 18:04:00 +0000

    Commit new version using maven + hibernate

---
+++ Bluetel/src/com/bluetel/entity/Customer.java
@@ -0,0 +1,70 @@
+package com.bluetel.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+
+@Entity
+public class Customer implements Serializable
+{
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int customerId;
+	private String serialNumber;
+	private String mpxn;
+	private String readDate;
+	
+	@OneToMany(mappedBy="customer", orphanRemoval=true)
+	@Cascade(value=CascadeType.ALL)
+	private List<Register> register;
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getSerialNumber() {
+		return serialNumber;
+	}
+
+	public void setSerialNumber(String serialNumber) {
+		this.serialNumber = serialNumber;
+	}
+
+	public String getMpxn() {
+		return mpxn;
+	}
+
+	public void setMpxn(String mpxn) {
+		this.mpxn = mpxn;
+	}
+
+	public String getReadDate() {
+		return readDate;
+	}
+
+	public void setReadDate(String readDate) {
+		this.readDate = readDate;
+	}
+
+	public List<Register> getRegister() {
+		return register;
+	}
+
+	public void setRegister(List<Register> register) {
+		this.register = register;
+	}
+}
---
+++ Bluetel/src/com/bluetel/entity/Register.java
@@ -0,0 +1,67 @@
+package com.bluetel.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.ForeignKey;
+
+@Entity
+public class Register
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	private String type;
+	private String registerId;
+	private String value;
+
+	@ManyToOne
+	@JoinColumn(name = "idCustomer", nullable = false)
+	@ForeignKey(name = "customer_fk")
+	private Customer customer;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getRegisterId() {
+		return registerId;
+	}
+
+	public void setRegisterId(String registerId) {
+		this.registerId = registerId;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+}
